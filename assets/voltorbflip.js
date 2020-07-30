ee4d41bd16bf9bdbb7fdbba742758f4b19b502bc
unknown <mfudg395@mtroyal.ca>
2020-07-29 18:26:44 -0600

    Implemented randomBetween() method

--- assets/voltorbflip.js
+++ assets/voltorbflip.js
@@ -1,6 +1,14 @@
-var score = parseInt(document.getElementById("score-num").textContent);
-var level = parseInt(document.getElementById("level-num").textContent);
+var score = parseInt(document.getElementById("score-num").textContent); // score starts at 0
+var level = parseInt(document.getElementById("level-num").textContent); // level starts at 1
+var gameBoard = [];
 
-function initGame() {
-    
+initGame();
+
+function initBoard() {
+
+}
+
+// Returns a random number between the specified minimum and maximum, inclusive.
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
