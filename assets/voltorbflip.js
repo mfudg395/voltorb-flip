429ace344bd38a7c094ec96836cb0b234df69da9
unknown <mfudg395@mtroyal.ca>
2020-08-01 11:00:33 -0600

    Implemented logic for finding number of 2s/3s to include

--- assets/voltorbflip.js
+++ assets/voltorbflip.js
@@ -1,10 +1,28 @@
+const rows = 5;
+const cols = 5;
+
 var score = parseInt(document.getElementById("score-num").textContent); // score starts at 0
 var level = parseInt(document.getElementById("level-num").textContent); // level starts at 1
 var gameBoard = [];
 
-initGame();
+initBoard();
 
 function initBoard() {
+    level = 8;
+    var tempBoard = []; // Temporary array to insert all the necessary values.
+
+    const numVoltorbs = randomBetween(level + 3, level + 5); // Number of Voltorbs to include in the board.
+    var numTwos = 0; // Number of twos and threes to include on the board.
+    var numThrees = 0; 
+
+    const minScore = 12 * Math.pow(2, level); // The minimum possible score you can get on a level.
+    var levelScore = 1; // The actual score you can get on the current level.
+
+    while (levelScore < minScore) {
+        var numToInclude = randomBetween(2, 3);
+        levelScore *= numToInclude;
+        numToInclude == 2 ? numTwos += 1 : numThrees += 1;
+    }
 
 }
 
